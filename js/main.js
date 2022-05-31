efe4ae99369be13c612c27e1066ee314219ad86b
Степан Кучеренко <s.kucherenko.pirit@yandex.ru>
2022-05-31 17:14:06 +0700

    Search, menu adaptive fixed

--- js/main.js
+++ js/main.js
@@ -6,24 +6,25 @@ window.addEventListener("DOMContentLoaded", function() {
   });
 
   document.addEventListener("click", function(e) {
+    e.preventDefault();
     let target = e.target;
     let form = document.querySelector(".search__form");
     if (!target.closest(".header__search") || target.closest(".search__close")) {
     form.classList.remove("search__form--active");
       form.querySelector("input").value = "";
-      document.querySelector(".search__btn--open").classList.remove("active")
+      document.querySelector(".search__btn--open").classList.remove("active");
     }
     if (!target.closest(".header-artists__list")) {
       document.querySelectorAll(".dropdown").forEach(el => {
           el.classList.remove("active-dropdown");
-      })
+      });
       document.querySelectorAll(".header-artists__btn").forEach(el => {
           el.classList.remove("dropdown__btn--active");
       });
     }
     if(target.closest(".header-main__item")) {
       document.querySelector(".header-main__nav").classList.remove("header-main__nav--active");
-      document.body.style.overflow = "";
+      document.body.classList.remove("no-scroll");
     }
   });
 
@@ -53,7 +54,7 @@ window.addEventListener("DOMContentLoaded", function() {
   // мобтльное меню
   document.querySelector(".burger").addEventListener("click", function() {
     document.querySelector(".header-main__nav").classList.add("header-main__nav--active");
-    document.body.style.overflow = "hidden";
+    document.body.classList.add("no-scroll");
   });
 
   // Слайдер
